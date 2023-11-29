a64d005b1092f1b07286d4401563090c5a52e9c6
dulanherath <dulanmherath@outlook.com>
2023-11-29 13:05:08 +0530

    video 6 half done, added eureka clients and done

--- employee-service/src/main/java/com/springbootacademy/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/springbootacademy/employeeservice/EmployeeServiceApplication.java
@@ -2,6 +2,7 @@ package com.springbootacademy.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableWebFlux
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
